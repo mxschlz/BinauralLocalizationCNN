b456c16dd932d369a9e6612eb0195058d032f8d3
Max Schulz <max.schulz10@web.de>
2023-10-05 14:31:52 +0200

    make train and test data

--- MSL/generate_numjudge_tfrecords_train.py
+++ MSL/generate_numjudge_tfrecords_train.py
@@ -4,6 +4,7 @@ import pickle
 from CNN_preproc import process_stims
 from stim_util import zero_padding
 from tfrecord_gen import create_tfrecord, check_record
+from CNN_util import get_dataset_partitions
 # from show_subbands import show_subbands
 
 # PIPELINE FOR NUMJUDGE TFRECORD GENERATION
@@ -11,13 +12,15 @@ from tfrecord_gen import create_tfrecord, check_record
 samplerate = 44100  # initial samplerate for CNN
 goal_duration = 2.1  # CNN processing goal duration
 
-# get stims from original experiment
-talkers_clear = pickle.load(open("/home/max/labplatform/sound_files/numjudge_talker_files_clear.pkl", "rb"))
-
 # simulate experiment sequence by rendering and adding sounds to create a complex multi-source environment
 # render the sound
 pos_azim = [-60, -40, -20, 0, 20, 40, 60]  # alternative: [-60, -40, -20, 0, 20, 40, 60]
 pos_elev = [0, 10, 20, 30, 40, 50, 60]  # alternative: [0, 10, 20, 30, 40, 50, 60]
+stim_n_reps = 10  # number of stimulus repetitions
+exp_n_reps = 18  # number of condition repetitions in the trial sequence --> 18 to match the experiment
+
+# get stims from original experiment
+talkers_clear = pickle.load(open("/home/max/labplatform/sound_files/numjudge_talker_files_clear.pkl", "rb"))
 
 # gather all available stimuli
 stimlist_clear = dict()
@@ -29,7 +32,7 @@ for talker in list(talkers_clear.keys()):
         stimlist_clear[talker].append(stim)
 
 
-sequence = slab.Trialsequence(conditions=[2, 3, 4, 5, 6], n_reps=1)
+sequence = slab.Trialsequence(conditions=[2, 3, 4, 5, 6], n_reps=exp_n_reps)
 talker_ids = list(stimlist_clear.keys())
 final_stims = list()
 
@@ -41,12 +44,12 @@ for i, _ in enumerate(sequence):
     azi = random.sample(pos_azim, n_sounds)
 
     for az, talker in zip(azi, talkers_this_trial):
-        stim = render_stims(random.choice(stimlist_clear[talker]), pos_azim=az, pos_elev=0, n_reps=1)
+        stim = render_stims(random.choice(stimlist_clear[talker]), pos_azim=az, pos_elev=0, n_reps=stim_n_reps)
         sound += slab.Binaural(data=stim[0]["sig"], samplerate=samplerate).resample(samplerate).resize(len(sound))
     sound = zero_padding(sound, goal_duration=goal_duration)
     # show_subbands(sound)
     final_stims.append({"sig": sound.data, "label": {"n_sounds": n_sounds, "sampling_rate": samplerate,
-                                                     "hrtf_idx": 0}})
+                                                     "hrtf_idx": 0, "azim": 1, "elev": 0}})
     # sound.play()
     # print("n_sounds", n_sounds)
 
@@ -54,24 +57,33 @@ for i, _ in enumerate(sequence):
     ele = random.sample(pos_elev, n_sounds)
 
     for el, talker in zip(ele, talkers_this_trial):
-        stim = render_stims(random.choice(stimlist_clear[talker]), pos_azim=0, pos_elev=el, n_reps=1)
+        stim = render_stims(random.choice(stimlist_clear[talker]), pos_azim=0, pos_elev=el, n_reps=stim_n_reps)
         sound += slab.Binaural(data=stim[0]["sig"], samplerate=samplerate).resample(samplerate).resize(len(sound))
     sound = zero_padding(sound, goal_duration=goal_duration)
 
     final_stims.append({"sig": sound.data, "label": {"n_sounds": n_sounds, "sampling_rate": samplerate,
-                                                     "hrtf_idx": 0}})
+                                                     "hrtf_idx": 0, "azim": 0, "elev": 1}})
     # show_subbands(sound)
     # sound.play()
     # print("n_sounds", n_sounds)
 
+# divide into train and test set
+train, test = get_dataset_partitions(final_stims, train_split=0.8, test_split=0.2, shuffle=True)
 # preprocessing
-stims_final = process_stims(final_stims)
+train_final = process_stims(train)
+test_final = process_stims(test)
+
 # write tfrecord
 rec_path = os.path.join("tfrecords/msl")
-rec_file = os.path.join(rec_path, f'numjudge_full_set_talkers_clear_train.tfrecords')
-create_tfrecord(stims_final, rec_file)
+rec_file_train = os.path.join(rec_path, f'numjudge_full_set_talkers_clear_train.tfrecords')
+rec_file_test = os.path.join(rec_path, f'numjudge_full_set_talkers_clear_test.tfrecords')
+create_tfrecord(train_final, rec_file_train)
+create_tfrecord(test_final, rec_file_test)
+
 # check record file
-status = check_record(rec_file)
+print("Training set length: ", len(train))
+print("Testing set length: ", len(test))
 
-
+print("TFrecords training set successful: ", check_record(rec_file_train))
+print("TFrecords testing set successful: ", check_record(rec_file_train))
 
