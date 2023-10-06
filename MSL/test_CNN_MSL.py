14c108bf0ca2a4fa42183bd66eda7684e8ae9d1e
Max Schulz <max.schulz10@web.de>
2023-10-06 16:39:02 +0200

    renaming

---
+++ MSL/test_CNN_MSL.py
@@ -0,0 +1,23 @@
+import os
+from run_CNN import run_CNN
+
+
+# LocaAccu tfrecs
+stim_tfrecs = os.path.join('tfrecords', 'msl', "locaaccu_noise_v.tfrecords")
+# trained net parameters
+for net in range(1, 11):
+    trainedNet = os.path.join('netweights', f'net{net}')
+    # result same name
+    res_name = os.path.join('Result', 'locaaccu_noise_v')
+    # run the model
+    run_CNN(stim_tfrecs, trainedNet, res_name)
+
+# LocaAccu tfrecs
+stim_tfrecs = os.path.join('tfrecords', 'msl', "*test.tfrecords")
+# trained net parameters
+for net in range(1, 11):
+    trainedNet = os.path.join('netweights_MSL', f'net{net}')
+    # result same name
+    res_name = os.path.join('Result', 'NumJudge_result')
+    # run the model
+    run_CNN(stim_tfrecs, trainedNet, res_name)
