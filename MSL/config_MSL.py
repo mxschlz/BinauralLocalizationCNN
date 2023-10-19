9887f58637650129e05c53f28b4739e0cfc80a80
mxschlz <schulz.max5@gmail.com>
2023-10-19 11:42:33 +0200

    create config file

---
+++ MSL/config_MSL.py
@@ -0,0 +1,18 @@
+CONFIG_TRAIN = dict(DEFAULT_NET_PARAM={'cpu_only': False, 'regularizer': None, "n_classes_localization": 5},
+                    DEFAULT_COST_PARAM={"multi_source_localization": True},
+                    DEFAULT_RUN_PARAM={'learning_rate': 1e-3,
+                                       'batch_size': 16,
+                                       'testing': False,
+                                       'model_version': ['100000'],  # orig model weights
+                                       "display_step": 1,
+                                       "total_steps": 10000,
+                                       "checkpoint_step": 10}
+                    )
+
+CONFIG_TEST = dict(DEFAULT_NET_PARAM={'cpu_only': False, 'regularizer': None, "n_classes_localization": 5},
+                   DEFAULT_COST_PARAM={"multi_source_localization": True},
+                   DEFAULT_RUN_PARAM={'learning_rate': 1e-3,
+                                      'batch_size': 16,
+                                      'testing': True,
+                                      'model_version': ['100000']}
+                   )
