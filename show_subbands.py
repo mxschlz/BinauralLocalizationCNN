64c302a6a39a2a0e98ce74d409d75c1029459b06
Max Schulz <max.schulz10@web.de>
2023-10-23 16:21:44 +0200

    show subbands bug fix

--- show_subbands.py
+++ show_subbands.py
@@ -9,9 +9,9 @@ def show_subbands(sig_bi):
                                        sig_bi.samplerate)
     subbands = cochleagram_wrapper(sig_norm[0], sig_norm[1], sliced=True)
     fig, ax = plt.subplots(3)
-    t = np.arange(48000) / 48000
-    ax[0].plot(t, sig_norm[0][0][1000:49000])
-    ax[0].plot(t, sig_norm[0][1][1000:49000])
+    t = np.arange(sig_bi.n_samples) / sig_bi.samplerate
+    ax[0].plot(sig_norm[0][0])
+    ax[0].plot(sig_norm[0][1])
     ax[0].set_ylabel('Amplitude (arb. unit)')
     ax[0].set_title("original signal")
     ax[0].legend(['left', 'right'])
@@ -24,7 +24,16 @@ def show_subbands(sig_bi):
     ax[2].imshow(subbands[:, :, 1], vmin=0, vmax=0.002, aspect='auto')
     ax[2].set_ylabel('Subbands')
     ax[2].set_title('right ear')
-    ax[2].set_xticklabels([-0.21, 0, 0.21, 0.42, 0.63, 0.84])
+    # ax[2].set_xticklabels([-0.21, 0, 0.21, 0.42, 0.63, 0.84])
     ax[2].set_xlabel('time (s)')
 
     plt.show()
+
+
+if __name__ == "__main__":
+    import slab
+    from stim_util import zero_padding
+    sig_bi = slab.Binaural.vowel(duration=1.0, samplerate=48000)
+    sig_bi = zero_padding(sig_bi, type="front", goal_duration=2.0)
+    sig_norm = normalize_binaural_stim(sig_bi.data, sig_bi.samplerate)
+    subbands = cochleagram_wrapper(sig_norm[0], sig_norm[1], sliced=True, minimum_padding=0.45)
