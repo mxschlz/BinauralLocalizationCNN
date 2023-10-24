e8c9a5aa54f0de93abe489405091bb21bd44d0b5
Max Schulz <max.schulz10@web.de>
2023-10-24 12:33:20 +0200

    add spectemp coverage

--- analysis_and_plotting/misc.py
+++ analysis_and_plotting/misc.py
@@ -1,3 +1,56 @@
+import numpy as np
+
+
+_p_ref = 2e-5  # 20 μPa, the standard reference pressure for sound in air
+
+
+def spectemp_coverage(sound_composition, dyn_range, upper_freq):
+    """
+    Calculate the spectro-temporal coverage of a sound composition.
+
+    Parameters:
+    - sound_composition (list): A list of sounds in the composition.
+    - dyn_range (float): The dynamic range in decibels (dB) to consider for coverage calculation.
+    - upper_freq (int): The upper frequency limit to consider for the analysis.
+
+    Returns:
+    - float: The ratio of the number of spectro-temporal points within the specified dynamic range
+             to the total number of spectro-temporal points in the composition.
+
+    Raises:
+    - ValueError: If the input sound_composition is not a list.
+
+    Note:
+    - This function assumes that the sounds in sound_composition have a spectrogram() method that
+      returns the spectrogram of the sound.
+
+    """
+    assert isinstance(sound_composition, list), ValueError("Input must be a list of sounds")
+
+    # Combine the sounds in the composition
+    sound = sum(sound_composition)
+
+    # Calculate the spectrogram and power
+    freqs, times, power = sound.spectrogram(show=False)
+    if upper_freq:
+        power = power[freqs < upper_freq, :]
+
+    # Convert power to logarithmic scale for plotting
+    power = 10 * np.log10(power / (_p_ref ** 2))
+
+    # Calculate the maximum and minimum dB values
+    dB_max = power.max()
+    dB_min = dB_max - dyn_range
+
+    # Select the interval of power values within the specified dynamic range
+    interval = power[np.where((power > dB_min) & (power < dB_max))]
+
+    # Calculate the ratio of points within the dynamic range to total points
+    coverage = interval.shape[0] / power.flatten().shape[0]
+
+    return coverage
+
+
 def get_azimuth_from_df(dataset):
     """
     Extracts azimuth values from a dataset and returns them as a list.
