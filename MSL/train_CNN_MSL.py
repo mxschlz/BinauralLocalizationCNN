71472e37fb971f0c697ddc630b917451b3a096e7
mxschlz <schulz.max5@gmail.com>
2023-10-26 12:12:09 +0200

    quick running loop

---
+++ MSL/train_CNN_MSL.py
@@ -0,0 +1,10 @@
+import os
+from run_CNN import run_CNN
+import MSL.config_MSL as cfg
+
+stim_tfrecs = os.path.join("*train*.tfrecords")
+# trained net parameters
+for net in range(2, 11):
+    trainedNet = os.path.join('netweights', f'net{net}')
+    # run the model
+    run_CNN(stim_tfrec_pattern=stim_tfrecs, trainedNet_path=trainedNet, cfg=cfg.CONFIG_TRAIN)
