51ffd72f3d9b85920be43334d7d1e2580e98bb5c
mxschlz <schulz.max5@gmail.com>
2023-10-31 12:47:11 +0100

    test cnn for msl paradigm

--- MSL/test_CNN_MSL.py
+++ MSL/test_CNN_MSL.py
@@ -1,5 +1,6 @@
 import os
 from run_CNN import run_CNN
+from MSL.config_MSL import CONFIG_TEST as cfg
 
 
 # LocaAccu tfrecs
@@ -13,12 +14,12 @@ for net in range(1, 11):
     run_CNN(stim_tfrecs, trainedNet, res_name)
 
 # MSL
-stim_tfrecs = os.path.join('tfrecords', 'msl', "numjudge_*test.tfrecords")
+stim_tfrecs = os.path.join("numjudge_*test_azi*.tfrecords")
 # trained net parameters
-for net in range(1, 2):
+for net in range(1, 11):
     trainedNet = os.path.join('netweights_MSL', f'net{net}')
     # result same name
     res_name = os.path.join('Result', 'NumJudge_result')
     # run the model
-    run_CNN(stim_tfrecs, trainedNet, res_name)
+    run_CNN(stim_tfrec_pattern=stim_tfrecs, trainedNet_path=trainedNet, save_name=res_name, cfg=cfg)
 
