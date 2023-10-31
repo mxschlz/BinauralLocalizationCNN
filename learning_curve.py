4a44becd4433dd1d6f4ed2c20d235202c4085466
mxschlz <schulz.max5@gmail.com>
2023-10-31 12:46:26 +0100

    plot average learning curve and individual curves for each network

--- learning_curve.py
+++ learning_curve.py
@@ -12,50 +12,55 @@ matplotlib.use("TkAgg")
 
 def get_learning_curve_json(root):
     """
-    Extracts learning curve data from JSON files in a directory and returns it as a Pandas DataFrame.
+    Extracts learning curve data from JSON files in a directory and returns it as a Pandas DataFrame
+    with a multi-index.
 
     Parameters:
     - root (str): The root directory containing subdirectories with JSON files.
 
     Returns:
-    - data (pd.DataFrame): A DataFrame with columns "iteration" and "accuracy" containing the learning curve data.
+    - data (pd.DataFrame): A DataFrame with multi-index ('network', 'iteration') and 'accuracy' columns.
     """
 
     # List subdirectories within the specified root directory
     children = sorted(os.listdir(root))
-    index = list(zip(*children))
-    multi_index = pd.MultiIndex.from_tuples(children)
 
-    # Create two DataFrames to store iteration and accuracy data
-    data_iterations = pd.DataFrame(columns=["iteration"])
-    data_accuracy = pd.DataFrame(columns=["accuracy"])
+    # Create an empty list to store dataframes for each network
+    network_dfs = []
 
     # Loop through subdirectories and process JSON files
     for d in children:
         full_path = os.path.join(root, d)
 
         # Find JSON files within the subdirectory
-        file = glob.glob(f"{full_path}/*json")
+        files = glob.glob(f"{full_path}/*json")
 
-        if file:
+        if files:
             # Load JSON content and extract iterations and accuracy values
-            content = np.array(json.load(open(file[0])))
+            content = np.array(json.load(open(files[0])))
             iterations = content[:, 0]
             accuracy = content[:, 1]
 
-            # Concatenate the extracted data into respective DataFrames
-            data_iterations = pd.concat([data_iterations, pd.DataFrame({"iteration": iterations})], ignore_index=True)
-            data_accuracy = pd.concat([data_accuracy, pd.DataFrame({"accuracy": accuracy})], ignore_index=True)
-        else:
-            # Skip directories without JSON files
-            pass
+            # Create a DataFrame for the current network
+            network_df = pd.DataFrame({'iteration': iterations, 'accuracy': accuracy})
 
-    # Combine the two DataFrames into one, resulting in a learning curve DataFrame
-    data = pd.concat([data_iterations, data_accuracy], axis=1)
+            # Add the network name as a new column in the DataFrame
+            network_df['network'] = d
+
+            # Append the DataFrame to the list
+            network_dfs.append(network_df)
+
+    # Concatenate all network DataFrames into one
+    data = pd.concat(network_dfs)
+
+    # Set the multi-index
+    data.set_index(['network'], inplace=True)
 
     return data
 
 
+fig, ax = plt.subplots(1, 2, sharex=True, sharey=True)
 data = get_learning_curve_json(root="netweights_MSL")
-sns.lineplot(data.iteration, data.accuracy)
+sns.lineplot(x="iteration", y="accuracy", data=data, ax=ax[0])
+sns.lineplot(x="iteration", y="accuracy", data=data, hue="network", ax=ax[1])
 plt.show()
