22b41cb18c5626a17dcb4991cdc44b8e9747dfb5
mxschlz <schulz.max5@gmail.com>
2023-11-27 17:11:29 +0100

    outsource decision rule

--- analysis_and_plotting/misc.py
+++ analysis_and_plotting/misc.py
@@ -118,13 +118,3 @@ def replace_in_array(array, to_replace_val=None, replace_with_val=0):
             array[i] = replace_with_val  # Replace the value at index i with replace_with_val
 
     return array
-
-
-def decide_sound_presence(cond_dis, criterion=0.09):
-    n_sounds_present = list()
-    for cd in cond_dis:
-        max_prob = cond_dis.max()
-        rule = max_prob * criterion
-        true_idx = np.where(cd >= rule)
-        n_sounds_present.append(len(true_idx[0]))
-    return n_sounds_present
