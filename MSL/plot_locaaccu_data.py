6ac199ecf8699ee0478ff92db62fea7dab3460e7
mxschlz <schulz.max5@gmail.com>
2023-12-18 11:10:08 +0100

    update

--- MSL/plot_locaaccu_data.py
+++ MSL/plot_locaaccu_data.py
@@ -1,29 +1,25 @@
 import matplotlib
 matplotlib.use("TkAgg")
-import matplotlib.pyplot as plt
 import seaborn as sns
-sns.set_theme()
+import matplotlib.pyplot as plt
+sns.set_theme(style="white", palette="viridis")
 from MSL.CNN_results_plot import results_locaaccu
 import os
 
 res_path = 'Result'
 
-# plot LocaAccu noise h
-model_data_pattern = os.path.join(res_path, "locaaccu_noise_h*")
-human_data_path = "tfrecords/msl/human_bevahiour/locaaccu_h.hdf"
-results_locaaccu(model_data_pattern, human_data_path, plane="h", stimtype="noise", plot_type="raincloud")
+model_data_pattern = os.path.join(res_path, "LocaAccu_babble_azi_result*")
+human_data_path = "human_bevahiour/locaaccu_h.hdf"
+results_locaaccu(model_data_pattern, human_data_path, plane="h", stimtype="babble", plot_type="reg")
 
-# plot LocaAccu noise v
-model_data_pattern = os.path.join(res_path, "locaaccu_noise_v*")
-human_data_path = "tfrecords/msl/human_bevahiour/locaaccu_v.hdf"
-results_locaaccu(model_data_pattern, human_data_path, plane="v", stimtype="noise", plot_type="raincloud")
+model_data_pattern = os.path.join(res_path, "LocaAccu_noise_azi_result*")
+human_data_path = "human_bevahiour/locaaccu_h.hdf"
+results_locaaccu(model_data_pattern, human_data_path, plane="h", stimtype="noise", plot_type="reg")
 
-# plot LocaAccu babble h
-model_data_pattern = os.path.join(res_path, "locaaccu_babble_h*")
-human_data_path = "tfrecords/msl/human_bevahiour/locaaccu_h.hdf"
-results_locaaccu(model_data_pattern, human_data_path, plane="h", stimtype="babble", plot_type="raincloud")
+model_data_pattern = os.path.join(res_path, "LocaAccu_babble_ele_result*")
+human_data_path = "human_bevahiour/locaaccu_h.hdf"
+results_locaaccu(model_data_pattern, human_data_path, plane="v", stimtype="babble", plot_type="reg")
 
-# plot LocaAccu babble v
-model_data_pattern = os.path.join(res_path, "locaaccu_babble_v*")
-human_data_path = "tfrecords/msl/human_bevahiour/locaaccu_v.hdf"
-results_locaaccu(model_data_pattern, human_data_path, plane="v", stimtype="babble", plot_type="raincloud")
+model_data_pattern = os.path.join(res_path, "LocaAccu_noise_ele_result*")
+human_data_path = "human_bevahiour/locaaccu_v.hdf"
+results_locaaccu(model_data_pattern, human_data_path, plane="v", stimtype="babble", plot_type="reg")
