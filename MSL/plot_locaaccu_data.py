416ac68daed15793d571ec88f8a62908c3eb0b73
mxschlz <schulz.max5@gmail.com>
2023-12-19 16:16:28 +0100

    update

--- MSL/plot_locaaccu_data.py
+++ MSL/plot_locaaccu_data.py
@@ -3,23 +3,19 @@ matplotlib.use("TkAgg")
 import seaborn as sns
 import matplotlib.pyplot as plt
 sns.set_theme(style="white", palette="viridis")
-from MSL.CNN_results_plot import results_locaaccu
+from MSL.CNN_results_plot import read_resfiles
+from stim_util import CNNpos_to_loc
 import os
+import numpy as np
 
 res_path = 'Result'
+model_data_pattern = os.path.join(res_path, "LocaAccu_babble_ele_result_net1*")
 
-model_data_pattern = os.path.join(res_path, "LocaAccu_babble_azi_result*")
-human_data_path = "human_bevahiour/locaaccu_h.hdf"
-results_locaaccu(model_data_pattern, human_data_path, plane="h", stimtype="babble", plot_type="reg")
 
-model_data_pattern = os.path.join(res_path, "LocaAccu_noise_azi_result*")
-human_data_path = "human_bevahiour/locaaccu_h.hdf"
-results_locaaccu(model_data_pattern, human_data_path, plane="h", stimtype="noise", plot_type="reg")
-
-model_data_pattern = os.path.join(res_path, "LocaAccu_babble_ele_result*")
-human_data_path = "human_bevahiour/locaaccu_h.hdf"
-results_locaaccu(model_data_pattern, human_data_path, plane="v", stimtype="babble", plot_type="reg")
-
-model_data_pattern = os.path.join(res_path, "LocaAccu_noise_ele_result*")
-human_data_path = "human_bevahiour/locaaccu_v.hdf"
-results_locaaccu(model_data_pattern, human_data_path, plane="v", stimtype="babble", plot_type="reg")
+header, data = read_resfiles(model_data_pattern)  # read results file
+data = np.concatenate(data, axis=0)  # concatenate data
+col_act = header.index('train/cnn_idx')  # get actual position column
+col_pred = header.index('model_pred')  # get predicted position
+loc_act = CNNpos_to_loc(data[:, col_act])[1]  # convert bin to azi, ele positions
+loc_pred = CNNpos_to_loc(data[:, col_pred])[1]
+sns.regplot(loc_act, loc_pred)
