256585fb2f5a6cd79870750b5d306b17b145c013
Max Schulz <max.schulz10@web.de>
2024-01-10 13:17:06 +0100

    update

--- show_subbands.py
+++ show_subbands.py
@@ -10,8 +10,8 @@ def show_subbands(sig_bi):
     subbands = cochleagram_wrapper(sig_norm[0], sig_norm[1], sliced=True)
     fig, ax = plt.subplots(3)
     t = np.arange(sig_bi.n_samples) / sig_bi.samplerate
-    ax[0].plot(sig_norm[0][0])
-    ax[0].plot(sig_norm[0][1])
+    ax[0].plot(t, sig_norm[0][0][1000:49000])
+    ax[0].plot(t, sig_norm[0][1][1000:49000])
     ax[0].set_ylabel('Amplitude (arb. unit)')
     ax[0].set_title("original signal")
     ax[0].legend(['left', 'right'])
@@ -40,42 +40,50 @@ if __name__ == "__main__":
     import seaborn as sns
     sns.set_theme(style="white")
     plt.rcParams['text.usetex'] = True  # TeX rendering
+    plt.ion()
 
     pos_azi = 90
     pos_ele = 30
-    samplerate = 48828
-    sig = slab.Sound.dynamic_tone(samplerate=samplerate)
-    sig = zero_padding(sig, type="frontback", goal_duration=2.1)
+    samplerate = 48000
+    sig = slab.Sound.vowel(duration=2.1)
+    # sig = zero_padding(sig, type="back", goal_duration=2.1)
     sig_bi = render_stims(orig_stim=sig, pos_elev=pos_ele, pos_azim=pos_azi)
     sound_bi = slab.Binaural(data=sig_bi[0]["sig"], samplerate=samplerate)
 
     sig_norm = normalize_binaural_stim(sound_bi.data,
                                        sound_bi.samplerate)
     subbands = cochleagram_wrapper(sig_norm[0], sig_norm[1], sliced=True)
+    t = np.arange(48000)/48000
 
     mosaic = """
     ad
     bb
-    cc"""
+    cc
+    """
     fig, ax = plt.subplot_mosaic(mosaic=mosaic)
-    ax["a"].plot(sig_norm[0][0])
-    ax["a"].plot(sig_norm[0][1])
+
+    ax["a"].plot(t, sig_norm[0][0][1000:49000])
+    ax["a"].plot(t, sig_norm[0][1][1000:49000])
     ax["a"].set_ylabel('Amplitude (arb. unit)')
-    ax["a"].set_title("Original signal")
-    ax["a"].set_xticklabels([0, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25])
+    # ax["a"].set_title("Original signal")
+    # ax["a"].set_xticks([])
     ax["a"].legend(['left', 'right'])
 
     ax["b"].imshow(subbands[:, :, 0], vmin=0, vmax=0.002, aspect='auto')
     ax["b"].set_ylabel('Subbands')
-    ax["b"].set_title('Left ear')
+    # ax["b"].set_title('Left ear')
+    # ax["b"].set_xticks([])
     ax["b"].tick_params(axis='x', labelbottom=False)
 
     ax["c"].imshow(subbands[:, :, 1], vmin=0, vmax=0.002, aspect='auto')
     ax["c"].set_ylabel('Subbands')
-    ax["c"].set_title('Right ear')
-    ax["c"].set_xticklabels([0, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25])
-    ax["c"].set_xlabel('time (s)')
+    # ax["c"].set_title('Right ear')
+    # ax["c"].set_xticks([])
+    ax["c"].set_xlabel('Time (s)')
 
     sound_bi.spectrum(axis=ax["d"])
     ax["a"].get_shared_x_axes().join(ax["b"], ax["c"])
-    plt.show()
+    ax["d"].set_xlabel("")
+    ax["d"].set_title("")
+    ax["c"].set_xticklabels([-0.2, 0, 0.2, 0.4, 0.6, 0.80])
+    # plt.tight_layout(pad=0.01)
