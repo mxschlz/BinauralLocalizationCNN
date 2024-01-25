ad10153dcee9483028f602cde73c24d01ab13af7
Max Schulz <max.schulz10@web.de>
2024-01-25 12:12:24 +0100

    save figure

--- MSL/eval_val_auc.py
+++ MSL/eval_val_auc.py
@@ -5,14 +5,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import scienceplots
 plt.style.use("science")
+plt.ion()
 import seaborn as sns
 
-# matplotlib.use("TkAgg")
+matplotlib.use("TkAgg")
 
 # get all auc files --> 10 nets, 10 files
 files = sorted(glob.glob("val_auc/*val_auc.npy"))
 # get all netnames
-netnames = [f.split("_val")[0] for f in files]
+netnames = [f.split("/")[1].split("_val")[0] for f in files]
 
 d = dict()
 
@@ -26,6 +27,12 @@ for file, net in zip(files, netnames):  # iterate through files
 df = pd.DataFrame(d)
 
 
-df.plot()
-plt.scatter(x=df.index, y=df.mean(axis=1))
-plt.show()
+x = df.index
+y = df.values
+df.plot(alpha=0.3, legend="")
+plt.plot(df.index, df.mean(axis=1), color="black")
+sns.scatterplot(x=df.index, y=df.mean(axis=1), color="black", marker="X", legend="")
+plt.vlines(x=14, ymin=plt.ylim()[0], ymax=df.mean(axis=1)[14], color="grey", linestyles="dashed")
+plt.hlines(y=df.mean(axis=1)[14], xmin=plt.xlim()[0], xmax=14, color="grey", linestyles="dashed")
+plt.xlabel("Training Level Per Hundred Steps")
+plt.ylabel("Area Under The Curve [Percent]")
