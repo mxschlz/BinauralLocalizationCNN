1872a1a3a4d307ea5bad3481c4faf70e2ffccb73
David Reinartz <d.c.reinartz@gmail.com>
2026-01-12 14:18:31 +0100

    Add layer index extraction during model conversion

--- blcnn/convert_models_to_tf2.py
+++ blcnn/convert_models_to_tf2.py
@@ -3,13 +3,15 @@ import sys
 from pathlib import Path
 
 import coloredlogs
+import keras
 import tensorflow as tf
+from keras import backend as K
 
-from net_builder import create_model
+from net_builder import create_model, create_checkpointed_model
 
 logger = tf.get_logger()
-logger.setLevel(logging.DEBUG)
-coloredlogs.install(level='DEBUG', logger=logger, fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger.setLevel(logging.INFO)
+coloredlogs.install(level='INFO', logger=logger, fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
 def main() -> None:
@@ -21,11 +23,18 @@ def convert_tf1_checkpoints_to_tf2_keras_models(path: Path) -> None:
     Args:
         path: Path to the directory containing the checkpoints for the 10 models.
     """
-    for i in range(1, 11):
+    layer_indices = {}
+
+    # for i in range(1, 11):
+    for i in range(1, 2):
         checkpoint_path = path / f'net{i}/model.ckpt-100000'
         logger.info(f'Converting checkpoint {checkpoint_path} to Keras model...')
-        _convert_ckpt_to_model(checkpoint_path, f'models/keras/net{i}.keras')
+        current_layer_indices = _convert_ckpt_to_model(checkpoint_path, f'models/keras/net{i}.keras')
         logger.info(f'Converted checkpoint for net{i} to Keras model at models/keras/net{i}.keras\n')
+        layer_indices[f'net{i}'] = current_layer_indices
+
+    with open(Path('models/keras/layer_indices.txt'), 'a') as f:
+        f.write(str(layer_indices) + '\n')
 
 
 def _convert_ckpt_to_model(checkpoint_path: Path, output_path: str) -> None:
@@ -54,9 +63,14 @@ def _convert_ckpt_to_model(checkpoint_path: Path, output_path: str) -> None:
         tf2_name = _replace_name(tf1_name)
 
         # Find the corresponding variable in the TF2 model
+        # TF2.14 -> e.g. name = 'conv2d_1/kernel:0'
+        # But TF2.16 -> e.g. name = 'kernel' ...
+        # We can use path in TF2.16 -> path = 'sequential/conv2d_1/kernel'
+
         tf2_variable = None
         for var in tf2_model.trainable_variables + tf2_model.non_trainable_variables:
-            if var.name.split(":")[0] == tf2_name:
+            if var.name.split(":")[0] == tf2_name:  # TF2.14
+            # if var.path.replace('sequential/', '') == tf2_name:  # TF2.16
                 tf2_variable = var
                 break
 
@@ -73,8 +87,22 @@ def _convert_ckpt_to_model(checkpoint_path: Path, output_path: str) -> None:
     Path(output_path).parent.mkdir(parents=True, exist_ok=True)
     tf2_model.save(output_path)
 
+    # Extract and save the indices of all Conv2D layers and the Dense layer
+    conv2d_indices = []
+    for i, layer in enumerate(tf2_model.layers):
+        if isinstance(layer, keras.layers.Conv2D):
+            conv2d_indices.append(i)
+        if layer.name == 'dense':
+            dense_index = i
+    logger.debug('Conv2D indices:', conv2d_indices)
+    logger.debug('Dense index:', dense_index)
+    indices = {'conv2d': conv2d_indices,
+               'dense': dense_index}
+
     # Clear the session to reset numbering of layers in the TF2 model
     tf.keras.backend.clear_session()
+
+    return indices
 
 
 def _replace_name(name: str) -> str:
